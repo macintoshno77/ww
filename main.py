a303080b1292c5c5845750c3bc340340bd9546b1
macintoshno77 <145097781+macintoshno77@users.noreply.github.com>
2024-03-02 21:45:43 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+answer = "TRAIN"
+
+@app.get('/answer')
+def get_answer():
+    return answer
+
+app.mount("/", StaticFiles(directory="wordle",html=True), name="wordle")
